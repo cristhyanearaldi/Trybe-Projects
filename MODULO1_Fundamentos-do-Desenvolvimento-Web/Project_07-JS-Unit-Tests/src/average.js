7c10c3d931532bf8c276c7d7eeed93dbbe8bfe50
Cristhyane G Araldi <85767928+CristhyaneAraldi@users.noreply.github.com>
2021-08-27 17:48:33 -0300

    Add project 07 - Unit Tests

---
+++ MODULO1_Fundamentos-do-Desenvolvimento-Web/Project_07-JS-Unit-Tests/src/average.js
@@ -0,0 +1,33 @@
+/*
+  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
+  Caso a função receba algum valor não númerico ou um array vazio,
+  o valor undefined deve ser retornado.
+  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+
+  Parâmetros:
+    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
+  Comportamento:
+    - average([2, 2]) // Retorno: 2;
+    - average([1, 1]) // Retorno: 1;
+    - average([1, '2']) // Retorno: undefined;
+*/
+
+const average = (array) => {
+  // add your implementation here
+ if (array.length === 0) {
+   return undefined;
+ }
+
+ let soma = 0;
+ let media = 0;
+ for (let index = 0; index < array.length; index += 1) {
+   if (typeof array[index] !== 'number') {
+     return undefined;
+   }
+   soma += array[index];
+   media = Math.round(soma / array.length);
+ }
+return media;
+};
+// Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+module.exports = average;
---
+++ MODULO1_Fundamentos-do-Desenvolvimento-Web/Project_07-JS-Unit-Tests/tests/productDetails.spec.js
@@ -0,0 +1,48 @@
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
+
+/*
+  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara') // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    const array1 = productDetails('Alcool gel', 'Máscara');
+    assert.strictEqual(typeof array1, 'object');
+    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(array1.length, 2);
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof array1[0], 'object');
+    assert.deepStrictEqual(typeof array1[1], 'object');
+    // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(array1[0], array1[1]); // array1[0] !== array1[1]
+    // Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(array1[0].details.productId, 'Alcool gel123');
+    assert.deepStrictEqual(array1[1].details.productId, 'Máscara123');
+  });
+});
